f6b5201329a9fdda7ec71c2533650069077ed661
clovisT2525 <clovisateh119@gmail.com>
2022-07-23 01:26:58 -0400

    updating pipeline

--- DeployScript .sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/clovisT2525/djanog-book-store.git
-cd /django-book-store
-docker-compose up -d
-docker ps
-docker-compose exec web python manage.py makemigrations --noinput
-docker-compose exec web python manage.py migrate --noinput
